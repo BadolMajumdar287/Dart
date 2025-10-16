4cec3d531d42c2b60a94db851dc683903d11708d
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-10-16 14:54:15 +0530

    dart function exicuted

---
+++ lib/language.dart
@@ -0,0 +1,91 @@
+void main() {
+  print("Hello Dart!");
+}
+
+
+
+// The main() function is a predefind function in Dart programming Language.
+// It is the entry point of every Dart application.
+// It is mandatory to have a main() function in every Dart progarm.
+// The main() function is executed when the program is run.
+// The main() funtion is defined using the keyword 'void'.
+// The main() funtion does not return any value.
+// The main() funtion can take optional arguments.
+// The main() function can be defined with or without parameters.
+// The main() funtion can be defined with or without return type.
+// The main() funtion can be defined with or without async keyword.
+// The main() funtion can be defined with or without async* keyword.
+// The main() funtion can be defined with or without async modifier.
+// The main() funtion can be defined with or without async* modifier.
+// The main() function can be defined with or without sync* modifier.
+// The main() function can be defiend with or without sync modifier.
+// The main() function can be defined with or without the 'new' keyword.
+// The main() function can be defiend with or without the 'await' keyword.
+// The main() function can be defined with or without the 'yield' keyword.
+// The main() function can be defiend with or without the 'yield*' keyword.
+// Parenthesis = ()
+// Curly Braces = {}
+// Square Brackets = []
+// Angle Brackets = <>
+// Single Quote = ''
+// Double Quote = ""
+// Backtick = ``
+// Semicolon = ;
+// Comma = ,
+// Period = .
+// Colon = :
+// Question Mark = ?
+// Exclamation Mark = !
+// Underscore = _
+// Dollar Sign = $
+// At Sign = @
+// Hash = #
+// Percent = %
+// Caret = ^
+// Ampersand = &
+// Asterisk = *
+// Plus = +
+// Minus = -
+// Equal sign = =
+// Tilde = ~
+// Modulus = %
+// Forward Slash = /
+// Pipe = /
+// BacksLash = \
+// Greater Then = >
+// Less Then = <
+// Single Line Comment = //
+// Multi Line Comment = /* */
+// Triple Quote = ''' or """
+// Triple Slash = ///
+// Triple Backtick = ```
+// Triple Greater Then = >>>
+// Triple Less Then = <<<
+// Arrow = =>
+// Double Ampersand = &&
+// Double pipe = ||
+// Double Equal = ==
+// Not Equal = !=
+// Triple Equal = ===
+// Plus Equal = +=
+// Minus Equal = -=
+// Multiply Equal = *=
+// Divide Equal = /=
+// Modulus Equal = %=
+// Increment = ++
+// Decrement = --
+// Null Aware = ??
+// Null Aware Assignment = ??=
+// Null Aware Access = ?.
+// Cascade Notation = .. or ?..
+// Spread Operator = ...
+// Type Test = is or is!
+// Negated Type Test = is!
+// As Typecast = as 
+// Await = await
+// Yield = yield or yield*
+// Lambda = =>
+// New = new
+// Async = async
+// Async* = async*
+// Sync* = sync*
