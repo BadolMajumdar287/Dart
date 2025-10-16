51123c2ddc641a3e99f0b534bfe703f99bef158b
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-10-16 21:25:19 +0530

    dartlist datatype

---
+++ lib/dataType/dartlist.dart
@@ -0,0 +1,13 @@
+//DART LISTS DATA TYPE
+// List is an ordered group of objects. It is similar to arrays in other programming Languages.
+
+void main() {
+  var integerList = [21, 32, 42, 12, 25];
+  var stringList = ['Apple', 'Banana', 'Mango', 'Orange'];
+  var mixedList = [21, 'Apple', 32, 'Banana', 42, 'Mango', true, false];
+
+  print(integerList[0]);
+  print(stringList);
+  print(mixedList);
+  print(mixedList.length);
+}
