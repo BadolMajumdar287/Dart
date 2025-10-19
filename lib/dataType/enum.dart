f488fa368a5b8842bb70f609ecca9cb6ccbc2499
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-10-19 17:23:23 +0530

    enum dataType

---
+++ lib/dataType/enum.dart
@@ -0,0 +1,5 @@
+//DART ENUM DATA TYPE
+// The enum data type in Dart is used to define a set of named constant values.
+// Enums are useful when you want to represent a fixed number of related values in a type-safe way.
+
+void main() {}
