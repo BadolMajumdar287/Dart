405521fa2925db2cf33080ef136199fcbe416b36
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-10-19 17:08:36 +0530

    dynamik dataType

--- lib/dataType/dynamic.dart
+++ lib/dataType/dynamic.dart
@@ -1,5 +1,17 @@
 //DART DYNAMIC DATA TYPE
-// The dynamic data type in Dart is a specaial type that can hold values of any type.
+// The dynamic data type in Dart is a specaial type that can hold values of any type (int, String, List, Map, etc).
 // A variable declared as dynamic can change its type at runtime.
 
-void main() {}
+void main() {
+  dynamic a = 10;
+  print(a); // Output = 10
+  a = "hello";
+  print(a); //Output = hello
+
+  a = [1, 2, 3, 5, 7];
+
+  print(a); // Output = [1,2,3,4,5];
+
+  Map<String, dynamic> d = {'name': 'badol', 'age': 25, 'city': "kolkata"};
+  print(d); // Output = {'name': 'badol', 'age': 25, 'city': "kolkata"};
+}
--- lib/dataType/set.dart
+++ lib/dataType/set.dart
@@ -2,7 +2,7 @@
 //The set data type in Dart is used to store a collection of unique items. Sets are unordered and do not allow duplicate values.
 
 void main() {
-  var myset1 = {10, 20, 30, 40, 10}; //Set of integers
+  Set myset1 = {10, 20, 30, 40}; //Set of integers
   print(myset1); //Output = {10,20,30,40}
 
   var myset2 = new Set(); //Creating an empty set and adding values to it
@@ -14,7 +14,7 @@ void main() {
   print(myset2); //Output = {100,200,300,400}
 
   var myset3 = new Set.from(
-    [1, 2, 3, 4, 5, 6, 1, 3],
+    [1, 2, 3, 4, 5, 6],
   ); //Creating a set from a list with duplicate values and the duplicates will be removed automatically .
   print(myset3); // Output = {1,2,3,4,5,6}
   print(myset3.length); // Output = 6
