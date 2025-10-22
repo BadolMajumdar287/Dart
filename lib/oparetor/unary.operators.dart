18bdf828119da25ea1e0e21a4e2bc1e9f25e825d
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-10-22 17:50:26 +0530

    Unary oparator

---
+++ lib/oparetor/unary.operators.dart
@@ -0,0 +1,32 @@
+//DART UNARY OPERATORS
+
+void main() {
+  // Unary Plus Operator // Increment Operator
+
+  int a = 10;
+
+  //Prefix
+
+  int b = ++a;
+  print(b);
+
+  // Postfix
+
+  int c = a++;
+
+  print(c);
+
+  // Unary Minus Operator // Decrement Operator
+
+  int f = 20;
+
+  //Prifix
+
+  int d = --f;
+  print(d);
+
+  //Postfix
+  int e = f--;
+
+  print(e);
+}
