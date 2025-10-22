7952ee6630a92423e3afadf419f81d34ad18f308
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-10-22 17:36:14 +0530

    Arithmetic Operators

---
+++ lib/oparetor/arithmetic.oparetor.dart
@@ -0,0 +1,14 @@
+//DART IMPORTANT ARITHMETIC OPERATORS
+
+void main() {
+  int a = 100;
+  int b = 50;
+
+  print("Addition: ${a + b}"); // Addition Operator
+  print("Subtraction: ${a - b}"); // Subtraction Operator
+  print("Multiplication: ${a * b}"); // Multiplication Operator
+  print("Division: ${a / b}"); // Division Operator
+  print("Integer Division: ${a ~/ b}"); // Integer Division Operator
+  print("Modulus: ${a % b}"); // Modulus Operator
+  print("Divide: ${a / b}"); // Divide Operator
+}
