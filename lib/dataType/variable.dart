07afd279ccbe456ff0507b4ad168e8504fd9eeb6
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-10-22 17:02:18 +0530

    add variable.dart

--- lib/dataType/variable.dart
+++ lib/dataType/variable.dart
@@ -1,9 +1,66 @@
 void main() {
-  var x = 10;
-  var y = 20;
+  // var kyword to declare a variable.
 
-  var z = x + y;
+  var x = 10; // interger variable
+  x = 20; // re-assigning value to variable
+  x = 30; // re-assigning value to variable
+  print(x);
+  // x = "Hello Dart"; // re- assigning value of different data type to variable.(This will give error)
+  // print(x);
+
+  // const kyword to declare a variable.
+
+  const y = 50; // interger constant variable
+  print(y);
+  //y = 60; // re-assigning value to constant variable. (This will give error)
+  //print(y);
+
+  // final kyword to declare a variable.
+
+  final z = 70; // interger final variable
   print(z);
+  //z = 80; // re-assigning value to final variable. (This will give error)
+  //print(z);
+
+  // dynamic kyword to declare a variable.
+
+  dynamic a = 100; //  interger dynamic variable
+  a = "dart"; // re-assigning value of different data type to dynamic variable.
+  print(a);
+
+  //String kyword to declare a variable.
+
+  String b = "hello"; // string variable.
+  b = "hello"; // re-assigning value to string variable.
+  print(b);
+  // b = 12; // re-assigning value of different data type to string variable.(This will give error)
+  //print(b);
+
+  //int kyword to declare a variable.
+
+  int c = 200; //int variable.
+  c = 300; // re-assigning value to interger variable.
+  print(c);
+  // c = "Dart"; // re-assigning value of different data type to interger variable.(This will give error);
+  // print(c);
+
+  //double kyword to declare a variable.
+
+  double d = 12.4; // double variable.
+  d = 15.6; // re-assigning value to double variable.
+  print(d);
+  d = 20; // re-assigning integer value to double variable.
+  print(d);
+  // d = "hello";  // re-assigning value of different data type to double variable.(This will give error);
+  // print(d);
+
+  //bool kyword to declare a variable.
+
+  bool e = true; // boolean variable.
+  e = false; // re-assigning value to boolean variable.
+  print(e);
+  // e = 20; // re-assigning value of different data type to boolean variable.(This will give error);
+  //print(e);
 }
 
 // Variable is used to store numeric values and refer to them later in the program.
