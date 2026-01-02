5f9618bfb2d2fbf318c1b5036291c5a5e0ead9aa
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-02 13:09:00 +0530

    ExportAndImportClass

---
+++ Class/ExportAndImportClass/importclass.dart
@@ -0,0 +1,12 @@
+import 'exportclass.dart';
+
+void main(List<String> args) {
+  var obj = new Myclass();
+
+  print(obj.name);
+  print(obj.age);
+  print(obj.student);
+  print(obj.MyList.length);
+  print(obj.MyMap);
+  print(obj.addTonumber(12, 12));
+}
