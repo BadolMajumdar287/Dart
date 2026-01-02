bdb3c28c62a3be1161308d865be1bfdd3400cfe7
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-02 22:33:56 +0530

    Map

---
+++ Map/map.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  var student = {
+    "name": "Badol Majumdar",
+    "age": 21,
+    "city": "Unknown",
+    "student": true,
+    "subject": "CST",
+  };
+
+  print(student);
+  print(student["age"]);
+  print(student["city"]);
+  print(student.length);
+  print(student.values);
+  print(student.keys);
+  print(student.hashCode);
+  print(student.entries);
+  print(student.runtimeType);
+  print(student.remove("age"));
+}
