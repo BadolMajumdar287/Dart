a734f835ddc94c4f65e5455c378d5b7150676c75
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-02 17:00:04 +0530

    Debugging

---
+++ Debugging/debug.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  var x = 20;
+  var y = 10;
+
+  var add = x + y;
+  var sub = x - y;
+  var mul = x * y;
+  var div = x / y;
+  var mod = x % y;
+
+  print(add);
+  print(sub);
+  print(mul);
+  print(div);
+  print(mod);
+}
