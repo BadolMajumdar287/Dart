3e5b6be31159c61356b7772a42f694a9b46fb060
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-02 13:45:57 +0530

    constructorClass

---
+++ Class/constructorClass/exportconstructorclass.dart
@@ -0,0 +1,11 @@
+class ConstructorClass {
+  ConstructorClass(message, name, age, student) {
+    print("Im a Badol Majumdar.");
+
+    print(10 + 20);
+    print(message);
+    print(name);
+    print(age);
+    print(student);
+  }
+}
---
+++ Class/constructorClass/importconstructorclass.dart
@@ -0,0 +1,5 @@
+import 'exportconstructorclass.dart';
+
+void main(List<String> args) {
+  ConstructorClass("I am constractor params.", "Badol Majumdar", 21, true);
+}
