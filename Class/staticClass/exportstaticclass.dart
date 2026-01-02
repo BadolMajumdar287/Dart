eb9442c42743c7da642f6484773ed223af83b44e
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-02 13:20:20 +0530

    Statik class

---
+++ Class/staticClass/exportstaticclass.dart
@@ -0,0 +1,27 @@
+class Statikclass {
+  static var name = "Badol Majumdar";
+
+  static var age = 20;
+
+  static var student = true;
+
+  static var MyList = [
+    "Badol",
+    "Shomrat",
+    "Shuvo",
+    "Digonto",
+    "Rudra",
+    "Shivom",
+  ];
+
+  static var MyMap = {
+    "name": "Badol Majumdar",
+    "age": 20,
+    "city": "Unknown",
+    "subject": "CST",
+  };
+
+  static addTwoNumber(int x, int y) {
+    print(x + y);
+  }
+}
---
+++ Class/staticClass/importstaticclass.dart
@@ -0,0 +1,11 @@
+import 'exportstaticclass.dart';
+
+void main(List<String> args) {
+  Statikclass();
+
+  print(Statikclass.addTwoNumber(10, 20));
+  print(Statikclass.student);
+  print(Statikclass.MyList);
+  print(Statikclass.age);
+  print(Statikclass.MyMap);
+}
