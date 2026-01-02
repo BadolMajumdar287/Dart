128ad61f5c069808a7faab647e8724107d1b13d5
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-02 16:15:43 +0530

    abstractclass

---
+++ Class/abstractclass/abstractclass.dart
@@ -0,0 +1,27 @@
+import '../inheritance/inheritanceclass.dart';
+
+abstract class father {
+  totalMoney() {
+    print("Money = 2000000");
+  }
+
+  totalLand() {
+    print("Land = 20 Acor");
+  }
+}
+
+class Son extends father {
+  totalMoney() {
+    print("Money = 1000000");
+  }
+
+  totalLand() {
+    print("Land = 30 Acor");
+  }
+}
+
+void main(List<String> args) {
+  var Sobj = Son();
+  Sobj.totalLand();
+  Sobj.totalMoney();
+}
