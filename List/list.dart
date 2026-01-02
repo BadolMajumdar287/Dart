8d8fa33cd585e286aa3a89fb6b066d8627f314d2
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-02 22:51:11 +0530

    List

---
+++ List/list.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  var RollNumbar = [1231, 1232, 1233, 1234, 1235, 1236, 1237, 1238, 1239, 1240];
+
+  print(RollNumbar);
+  print(RollNumbar.length);
+  print(RollNumbar.first);
+  print(RollNumbar.last);
+  print(RollNumbar.lastIndexOf(1231));
+  print(RollNumbar.lastIndexOf(1238));
+  print(RollNumbar[3]);
+  print(RollNumbar[5]);
+  print(RollNumbar.hashCode);
+}
