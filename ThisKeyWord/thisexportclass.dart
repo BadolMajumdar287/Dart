9e077ab64fb67046bfe44440520a7ca2f55f05ed
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-02 14:16:33 +0530

    ThisKeyWord

---
+++ ThisKeyWord/thisexportclass.dart
@@ -0,0 +1,17 @@
+class Thisclass {
+  var num1 = 20;
+  var num2 = 20;
+
+  addTonumber() {
+    print(this.num1 + this.num2);
+  }
+
+  multipleTwonumber() {
+    print(3 - 2);
+  }
+
+  result() {
+    multipleTwonumber();
+    print(this.num1 - this.num2);
+  }
+}
---
+++ ThisKeyWord/thisimportclass.dart
@@ -0,0 +1,9 @@
+import 'thisexportclass.dart';
+
+void main(List<String> args) {
+  var obj = new Thisclass();
+
+  print(obj.addTonumber());
+
+  obj.result();
+}
