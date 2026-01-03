d0b3ca28861daa3f8d0ed05d4422940cc39e447e
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-03 14:05:06 +0530

    define.calling.function.dart

---
+++ Function/define.calling.function.dart
@@ -0,0 +1,21 @@
+//DART FUNCTION
+
+//Return_Type: It can be any data type such as void,integer,float etc. The return type must be matched with the returnd value of the function.
+//Function_Name: It should be an appropriate and void identifier.
+//Parameter_List: It denotes teh list of the parameters,Which is necessary when we called a function.
+//Return_Value: A function Return a value after complete its execution.
+
+void main() {
+  //DEFINING A FUNCTION
+  //A function can be defined by providing the anme of the function with the appropriate parameter and return type.
+  //A function contains a set statements which are called function body.
+  addTwoNumber() {
+    var x = 10;
+    var y = 10;
+    print(x + y);
+  }
+
+  //CALLING A FUNCTION
+  //After creating a function,we can call or invoke the defined funtion inside the main() function body.
+  addTwoNumber();
+}
