d4ede4eef1364864e78abbe98b8c5d9be328eb58
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-03 14:07:14 +0530

    passing.arguments.to.function.dart

---
+++ Function/passing.arguments.to.function.dart
@@ -0,0 +1,12 @@
+//PASSING ARGUMENTS TO FUNCTIONS
+//When a function is called,it may have some information as per the function protype is known as a parameter(arguments).
+
+void main() {
+  multipleNmber(int x, int y) {
+    print(x * y);
+  }
+
+  multipleNmber(10, 10);
+  multipleNmber(10, 12);
+  multipleNmber(20, 20);
+}
