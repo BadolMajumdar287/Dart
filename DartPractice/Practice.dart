2ebc4e09081162b10bd9fe39c3200b61a0164a2a
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-03 21:09:57 +0530

    DartPractice

---
+++ DartPractice/Practice.dart
@@ -0,0 +1,108 @@
+//DART PRACTICE
+
+void main(List<String> args) {
+  // print("Hello, Dart!");
+
+  //FUNCTION PRACTICE
+
+  // Functionpractice(int a, int b) {
+  //   // print(a + b);
+  //   return a + b;
+  // }
+
+  // var result = Functionpractice(12, 12);
+  // print(result);
+
+  // IF-ELSE PRACTICE
+
+  // var number = -10;
+  // if (0 < number) {
+  //   print("The number is positive.");
+  // } else {
+  //   print("The number is not positive.");
+  // }
+
+  // var markes = 60;
+
+  // if (markes >= 80) {
+  //   print("Grade A+");
+  // } else if (markes >= 70) {
+  //   print("Garde A");
+  // } else if (markes >= 60) {
+  //   print("Grade A-");
+  // } else if (markes >= 50) {
+  //   print("Grade B");
+  // } else if (markes >= 40) {
+  //   print("Grade C");
+  // } else {
+  //   print("Fail");
+  // }
+
+  //LOOP PRACTICE
+
+  // for (var i = 1; i <= 100; i++) {
+  //   print("print number 1 to 100: $i");
+  // }
+
+  // for (var i = 1; i < 100; i = i + 2) {
+  //   print("print number odd 1 to 100: $i");
+  // }
+
+  // for (var i = 2; i < 100; i = i + 2) {
+  //   print("print number even 1 to 100: $i");
+  // }
+
+  // var number = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+  // for (var element in number) {
+  //   print(element);
+  // }
+
+  //LIST AND MAP PRACTICE
+
+  // var StudentDetail = [
+  //   {"name": "Badol", "age": "21", "city": "Unknown"},
+  //   {"name": "Somrat", "age": "30", "city": "Unknown"},
+  //   {"name": "Rudra", "age": "22", "city": "Unknown"},
+  //   {"name": "Akash", "age": "24", "city": "Unknown"},
+  //   {"name": "Plash", "age": "22", "city": "Unknown"},
+  //   {"name": "Dipto", "age": "20", "city": "Unknown"},
+  // ];
+
+  // for (var element in StudentDetail) {
+  //   // print(element);
+
+  //   print(
+  //     "Student name is ${element["name"]} and age ${element["age"]} and city name ${element["city"]}",
+  //   );
+  // }
+
+  //LIST PRACTICE
+
+  // var number2 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+  // print(number2.length);
+  // print(number2.reversed);
+  // print(number2.first);
+  // print(number2.last);
+  // print(number2[3]);
+  // print(number2.indexOf(40));
+  // print(number2.isEmpty);
+  // print(number2.isNotEmpty);
+  // print(number2.lastIndexOf(90));
+  // print(number2.hashCode);
+
+  // MAP PRACTICE
+
+  // var student = {"name": "Badol", "age": "21", "city": "Unknown"};
+  // student["country"] = "India";
+  // print(student);
+  // print(student.values);
+  // print(student.keys);
+  // print(student.length);
+  // print(student.isEmpty);
+  // print(student.isNotEmpty);
+  // print(student.hashCode);
+  // student.remove("city");
+  // print(student);
+}
