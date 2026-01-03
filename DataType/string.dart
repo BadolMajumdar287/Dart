0493e2e177546e022b44c4c62afab47e8a1f907b
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-03 14:17:18 +0530

    string

---
+++ DataType/string.dart
@@ -0,0 +1,13 @@
+//DART STRING DATA TYPE
+
+void main() {
+  var str1 = 'Hello';
+  var str2 = "Dart";
+  var str3 = '''Welcome to Dart programming''';
+  var str4 = """My country is India""";
+
+  print(str1);
+  print(str2);
+  print(str3);
+  print(str4);
+}
