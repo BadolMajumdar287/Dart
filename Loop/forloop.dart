e42925530f318efc26278ad1b30c0237b7cc57e9
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-03 16:35:37 +0530

    forloop

---
+++ Loop/forloop.dart
@@ -0,0 +1,9 @@
+// DART FOR LOOP
+
+//The for loop used when we known how many times a block of code will excute.
+
+void main(List<String> args) {
+  for (var i = 0; i <= 5; i++) {
+    print(i);
+  }
+}
