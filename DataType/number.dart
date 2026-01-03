ec5cf5b6ebe013b68e773b7bd6adca60a8bdd0de
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2026-01-03 14:14:54 +0530

    number

---
+++ DataType/number.dart
@@ -0,0 +1,15 @@
+//DART NUMBER DATA TYPE
+
+void main() {
+  var num1 = 10; //integer
+  var num2 = 5.5; //double
+
+  int a = 23;
+  double b = 1.2;
+
+  var add1 = a + b;
+  var add2 = num1 + num2;
+
+  print(add1);
+  print(add2);
+}
